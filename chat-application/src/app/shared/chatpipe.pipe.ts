95fab779017cf0fc372f482556244eb8a1385db6
Prabhleen-kaur <40427440+Prabhleen-kaur@users.noreply.github.com>
2018-08-30 11:08:08 +0530

    Update chatpipe.pipe.ts

--- chat-application/src/app/shared/chatpipe.pipe.ts
+++ chat-application/src/app/shared/chatpipe.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ChatpipePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string, character?: string): string {
+    return value.replace(character, '');
   }
 
 }
